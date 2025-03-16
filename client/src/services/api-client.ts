4fe38934960fe93e9db0f4437ff86bce8aa20f27
Raz Alon <71208052+razalon72@users.noreply.github.com>
2025-03-16 20:15:12 +0200

    Update BASE URL

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -4,7 +4,8 @@ export { CanceledError };
 const apiClient = axios.create({
   // baseURL: "https://10.10.246.48",
   // baseURL: 'https://193.106.55.100',
-    baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+    baseURL: "https://localhost:443",
 });
 
 export default apiClient;
