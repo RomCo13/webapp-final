485bff69b3ba718ac5dfbd0a5b0cae11a67bd031
Raz Alon <71208052+razalon72@users.noreply.github.com>
2025-03-16 20:27:19 +0200

    Update api-client.ts

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -5,7 +5,7 @@ const apiClient = axios.create({
   // baseURL: "https://10.10.246.48",
   // baseURL: 'https://193.106.55.100',
   // baseURL: "http://localhost:3000",
-    baseURL: "https://localhost:443",
+    baseURL: "https://193.106.55.208",
 });
 
 export default apiClient;
